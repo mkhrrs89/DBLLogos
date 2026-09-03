d520f4ddd84ac43d1dedeb75c5d6f36e920394f8
mkhrrs89 <47932065+mkhrrs89@users.noreply.github.com>
2026-09-03 11:32:19 -0400

    Refresh Draft Prospects loader for watch list labels

--- rivals.js
+++ rivals.js
@@ -223,7 +223,7 @@
 (() => {
   if (document.querySelector('script[data-dbl-draft-loader]')) return;
   const script = document.createElement('script');
-  script.src = './draft-prospects-loader.js?v=20260903-draft-class-filter';
+  script.src = './draft-prospects-loader.js?v=20260903-watch-list-labels';
   script.dataset.dblDraftLoader = 'true';
   document.body.append(script);
 })();
