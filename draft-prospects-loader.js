df57655260b698d67cdd51f973b0a215c93d276c
mkhrrs89 <47932065+mkhrrs89@users.noreply.github.com>
2026-09-03 10:28:47 -0400

    Refresh persistent Draft Prospects assets

--- draft-prospects-loader.js
+++ draft-prospects-loader.js
@@ -3,7 +3,7 @@
 
   const styleLink = document.createElement('link');
   styleLink.rel = 'stylesheet';
-  styleLink.href = './draft-prospects.css?v=20260902-draft-prospects-age-watch-colors';
+  styleLink.href = './draft-prospects.css?v=20260903-draft-prospects-persistent';
   document.head.append(styleLink);
 
   const fullscreenStyle = document.createElement('style');
@@ -73,7 +73,7 @@
   else page?.append(panel);
 
   const script = document.createElement('script');
-  script.src = './draft-prospects.js?v=20260902-draft-prospects-age-watch-colors';
+  script.src = './draft-prospects.js?v=20260903-draft-prospects-persistent';
   script.dataset.dblDraftProspects = 'true';
   document.body.append(script);
 })();
