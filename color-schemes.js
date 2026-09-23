dd48b221f0e1bb50edc9ee4422a2bccabe2cbcb9
mkhrrs89 <47932065+mkhrrs89@users.noreply.github.com>
2026-09-22 21:54:35 -0400

    Classify F3DC9B as White Cream

--- color-schemes.js
+++ color-schemes.js
@@ -92,6 +92,7 @@
     '#E4DAC0': 'White/Cream',
     '#F0DFBB': 'White/Cream',
     '#F3EDDB': 'White/Cream',
+    '#F3DC9B': 'White/Cream',
     '#657A85': 'Blue',
     '#953917': 'Red',
     '#973B03': 'Red',
