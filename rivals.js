6721ca7d22280b3d0ad225cc7fe766d86eb13634
mkhrrs89 <47932065+mkhrrs89@users.noreply.github.com>
2026-09-24 22:00:01 -0400

    Cache-bust Draft Prospects potential filter loader

--- rivals.js
+++ rivals.js
@@ -223,7 +223,7 @@
 (() => {
   if (document.querySelector('script[data-dbl-draft-loader]')) return;
   const script = document.createElement('script');
-  script.src = './draft-prospects-loader.js?v=20260924-duplicate-names';
+  script.src = './draft-prospects-loader.js?v=20260924-pot65-filter';
   script.dataset.dblDraftLoader = 'true';
   document.body.append(script);
 })();
