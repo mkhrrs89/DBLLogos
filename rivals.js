79d0605136afe70fd83da67c6321159953233902
mkhrrs89 <47932065+mkhrrs89@users.noreply.github.com>
2026-09-25 23:59:03 -0400

    Cache-bust Draft Prospects Pot 70 loader

--- rivals.js
+++ rivals.js
@@ -223,7 +223,7 @@
 (() => {
   if (document.querySelector('script[data-dbl-draft-loader]')) return;
   const script = document.createElement('script');
-  script.src = './draft-prospects-loader.js?v=20260924-pot65-filter';
+  script.src = './draft-prospects-loader.js?v=20260925-pot70-filter';
   script.dataset.dblDraftLoader = 'true';
   document.body.append(script);
 })();
